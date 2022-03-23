c693f245eabde1193ad1fa015002e2a081e3149e
Renan Andrade <RENANAN@dpc.ad>
2022-03-23 09:47:12 -0300

    Alterado os modulos para serem componentes do app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApadrinhamentoComponent } from './apadrinhamento/apadrinhamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'Apadrinhamento', component:ApadrinhamentoComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { ApadrinhamentoModule } from './apadrinhamento/apadrinhamento.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderModule } from './header/header.module';
-import { LoginModule } from './login/login.module';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { ApadrinhamentoComponent } from './apadrinhamento/apadrinhamento.component'
+import { ApadrinhamentoFormComponent } from './apadrinhamento/apadrinhamento-form/apadrinhamento-form.component';
+
+
+import { PanelModule } from 'primeng/panel';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { FileUploadModule } from 'primeng/fileupload';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    ApadrinhamentoComponent,
+    ApadrinhamentoFormComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HeaderModule,
-    ApadrinhamentoModule,
-    LoginModule
+    PanelModule,
+    CardModule,
+    ButtonModule,
+    DialogModule,
+    ToolbarModule,
+    InputTextareaModule,
+    InputTextModule,
+    DropdownModule,
+    FileUploadModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
